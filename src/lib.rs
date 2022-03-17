1f06b8eb4b65d14b3d49922fdc12cb76bded62f2
vmoiseev <vmoiseev@ro-nso.ru>
2022-03-17 20:01:16 +0700

    Step 1 end

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,12 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
 
 static SERVER_COUNTER: AtomicUsize = AtomicUsize::new(0);
+
+struct AppState{
+    server_id: usize,
+    request_count: Cell<usize>,
+    messages: Arc<Mutex<Vec<String>>>,
+}
 #[derive(Serialize)]
 struct IndexResponse{
     message: String,
