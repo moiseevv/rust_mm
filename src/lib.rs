85664b3d518811237e61346e7bd1eef4c8d80df5
vmoiseev <vmoiseev@ro-nso.ru>
2022-03-18 21:28:18 +0700

    request_count

--- src/lib.rs
+++ src/lib.rs
@@ -107,8 +107,8 @@ struct LookupResponse{
 
 #[get("/lookup/{index}")]
 fn lookup(state: web::Data<AppState>, idx: web::Path<usize>) -> Result<web::Json<LookupResponse>>{
-    let request_cont = state.request_count.get() + 1;
-    state.request_count.set(request_cont);
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
     let ms = state.messages.lock().unwrap();
     let result = ms.get(idx.into_inner()).cloned();
     Ok(web::Json(LookupResponse{
